4cf2f8f30ee96341f82ab7be50b3c98ec47bdb4b
Max <smolinskijmaksim1@gmail.com>
2025-07-06 16:00:34 +0100

    Move SECRET_KEY to environment variable via DJANGO_SECRET_KEY

--- fly_port_api/settings.py
+++ fly_port_api/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)(vyd&264s$hngu8*%hr^_1ts555gi5ylse)rdjq*ay2qv*4-$"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
